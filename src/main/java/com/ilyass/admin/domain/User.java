57a07e3ad9e70f0edb7dc3bb982318db869f654a
dell <dell@DESKTOP-4P8U8AR>
2024-07-17 13:41:44 +0100

    User entity

---
+++ src/main/java/com/ilyass/admin/domain/User.java
@@ -0,0 +1,22 @@
+package com.ilyass.admin.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+    private Long Id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private String profileImageUrl;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joinDate;
+    private String [] roles;
+    private String [] authorities;
+    private boolean isActive;
+    private boolean isNotLocked;
+}
