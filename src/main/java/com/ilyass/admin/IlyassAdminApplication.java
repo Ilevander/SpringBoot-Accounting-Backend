51eb82f23f41965eb84e04b64831db531b7c270d
dell <dell@DESKTOP-4P8U8AR>
2024-07-17 12:57:39 +0100

    initializing Spring Boot project

---
+++ src/main/java/com/ilyass/admin/IlyassAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ilyass.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlyassAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlyassAdminApplication.class, args);
+    }
+
+}
