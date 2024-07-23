cad058c1cf74525c214aa3396d90625688740ffc
dell <dell@DESKTOP-4P8U8AR>
2024-07-23 18:15:46 +0100

    Security: JWT Authorization Filter 2

--- src/main/java/com/ilyass/admin/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/ilyass/admin/filter/JwtAuthorizationFilter.java
@@ -5,6 +5,10 @@ import com.ilyass.admin.utility.JWTTokenProvider;
 import com.mysql.cj.x.protobuf.Mysqlx;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -12,9 +16,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 import static com.ilyass.admin.constant.SecurityConstant.TOKEN_PREFIX;
 
+@Component
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     private JWTTokenProvider jwtTokenProvider;
@@ -35,6 +41,17 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                        filterChain.doFilter(request,response);
                        return;
                    }
+                   String token = authorizationHeader.substring(TOKEN_PREFIX.length());
+                   String username = jwtTokenProvider.getSubject(token);
+                   if (jwtTokenProvider.isTokenValid(username,token) && SecurityContextHolder.getContext().getAuthentication() == null) {
+                       List<GrantedAuthority> authorities = jwtTokenProvider.getAuthorities(token);
+                       Authentication authentication = jwtTokenProvider.getAuthentication(username , authorities , request);
+                       SecurityContextHolder.getContext().setAuthentication(authentication);
+                   }
+                    else {
+                        SecurityContextHolder.clearContext();
+                   }
              }
+               filterChain.doFilter(request, response);
     }
 }
