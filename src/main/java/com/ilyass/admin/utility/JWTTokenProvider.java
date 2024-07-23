3a682624cb80f36fbe7080b4a139f75a16c28006
dell <dell@DESKTOP-4P8U8AR>
2024-07-23 17:13:00 +0100

    Security: JWT Token ProviderÃ

--- src/main/java/com/ilyass/admin/utility/JWTTokenProvider.java
+++ src/main/java/com/ilyass/admin/utility/JWTTokenProvider.java
@@ -4,22 +4,27 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.JWTVerifier;
-import com.ilyass.admin.constant.SecurityConstant;
 import com.ilyass.admin.domain.UserPrincipal;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 import static com.ilyass.admin.constant.SecurityConstant.*;
 import static java.util.Arrays.stream;
 
+@Component
 public class JWTTokenProvider {
     @Value("${jwt.secret}")
     private String secret;
@@ -37,13 +42,34 @@ public class JWTTokenProvider {
         return stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
     }
 
+    public Authentication getAuthentication(String username , List<GrantedAuthority> authoritites , HttpServletRequest request){
+        UsernamePasswordAuthenticationToken usernamePasswordAuthToken = new UsernamePasswordAuthenticationToken(username , null , authoritites);
+        usernamePasswordAuthToken.setDetails((new WebAuthenticationDetailsSource().buildDetails(request)));
+        return usernamePasswordAuthToken;
+    }
+
+    public boolean isTokenValid(String username , String token){
+        JWTVerifier verifier = getJWTVerifier();
+        return StringUtils.isNotEmpty(username) && isTokenExpired(verifier,token);
+    }
+
+    public String getSubject(String token){
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getSubject();
+    }
+
+    private boolean isTokenExpired(JWTVerifier verifier ,String token) {
+        Date expiration = verifier.verify(token).getExpiresAt();
+        return expiration.before(new Date());
+    }
+
     private String[] getClaimsFromToken(String token) {
         JWTVerifier verifier = getJWTVerifier();
         return verifier.verify(token).getClaim(AUTHORITIES).asArray(String.class);
     }
 
-    private com.auth0.jwt.interfaces.JWTVerifier getJWTVerifier() {
-        com.auth0.jwt.interfaces.JWTVerifier verifier;
+    private JWTVerifier getJWTVerifier() {
+        JWTVerifier verifier;
         try {
             Algorithm algorithm = HMAC512(secret);
             verifier = JWT.require(algorithm).withIssuer(GET_ARRAYS_LLC).build();
