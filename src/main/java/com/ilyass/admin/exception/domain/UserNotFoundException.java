58185d67b95172e40816bff4f1ea899168a78a6f
dell <dell@DESKTOP-4P8U8AR>
2024-07-28 15:25:22 +0100

    Custom Exception Handling: Custom exception classes

---
+++ src/main/java/com/ilyass/admin/exception/domain/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ilyass.admin.exception.domain;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
