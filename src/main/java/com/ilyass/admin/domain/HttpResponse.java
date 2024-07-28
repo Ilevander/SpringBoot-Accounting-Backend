60b899459b7d2a6bc97114e40e9fc1ca9fddf8e3
dell <dell@DESKTOP-4P8U8AR>
2024-07-28 15:07:17 +0100

    Security: Set timeStamp morocco , test endpoint for security

--- src/main/java/com/ilyass/admin/domain/HttpResponse.java
+++ src/main/java/com/ilyass/admin/domain/HttpResponse.java
@@ -1,20 +1,33 @@
 package com.ilyass.admin.domain;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
+import java.util.Date;
+
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class HttpResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss",timezone = "Morocco/Rabat")
+    private Date timestamp;
     private int httpStatusCode;
     private HttpStatus httpStatus;
     private String reason;
     private String message;
 
+    public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.timestamp = new Date();
+        this.httpStatusCode = httpStatusCode;
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+        this.message = message;
+    }
 
 }
+
+
+
+
